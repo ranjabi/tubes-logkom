1604f79a0d7d29166abc560833f54572e84d967c
dikyrest <16520040@std.stei.itb.ac.id>
2021-11-15 23:03:51 +0700

    create map and move

--- map.pl
+++ map.pl
@@ -1,12 +1,145 @@
-% Fakta terkait peta diky
-min_panjang(10).
-min_lebar(10).
-pemain('P').
-pagar('#').
-marketplace('M').
-ranch('R').
-house('H').
-quest('Q').
-tile('-').
-water_tile('o').
-digged_tile('=').
+/* File map.pl */
+/* Menyimpan ukuran peta, objek pada peta, dan perintah cetak peta */
+
+:- dynamic(map_object/3).
+
+/* map_size(X,Y) = X adalah lebar dan Y adalah tinggi 8/
+map_size(14,14).
+
+/* map_object(X, Y, Obj) = Object Obj di posisi (X,Y), X dihitung dari kiri, Y dihitung dari atas */
+map_object(5, 8, 'o').
+map_object(6, 8, 'o').
+map_object(7, 8, 'o').
+map_object(4, 9, 'o').
+map_object(5, 9, 'o').
+map_object(6, 9, 'o').
+map_object(7, 9, 'o').
+map_object(8, 9, 'o').
+map_object(5, 10, 'o').
+map_object(6, 10, 'o').
+map_object(7, 10, 'o').
+
+/* Perintah map untuk mencetak peta */
+map :-
+	isStart(true), !,
+	draw_map.
+
+map :- !,
+	write('Game has not started, use \"start.\" to play the game"').
+
+/* Mencetak tepi peta */
+/* Tepi kanan */
+draw_point(X, Y) :- map_size(W, H),
+					X =:= W + 1,
+					Y =< H + 1,
+					write('#'), nl,
+					NewY is Y+1,
+					draw_point(0, NewY).
+		
+/* Tepi kiri */		
+draw_point(X, Y) :- map_size(_, H),
+					X =:= 0,
+					Y =< H+1,
+					write('#'),
+					NewX is X+1,
+					draw_point(NewX, Y).
+				
+/* Tepi atas */				
+draw_point(X, Y) :- map_size(W, _),
+					X < W + 1,
+					X > 0,
+					Y =:= 0,
+					write('#'),
+					NewX is X+1,
+					draw_point(NewX, Y).
+					
+/* Tepi bawah */			
+draw_point(X, Y) :- map_size(W, H),
+					X < W + 1,
+					X > 0,
+					Y =:= H + 1,
+					write('#'),
+					NewX is X+1,
+					draw_point(NewX, Y).					
+				
+/* Mencetak bagian dalam peta */
+/* Jika pemain berada di lokasi spesial, maka simbol lokasi spesial akan tertimpa oleh simbol pemain */
+draw_point(X, Y) :- map_size(W, H),
+					X < W + 1,
+					X > 0,
+					Y < H + 1,
+					Y > 0,
+					map_object(X, Y, 'P'),
+					map_object(X, Y, 'M'), !,
+					write('P'),
+					NewX is X+1,
+					draw_point(NewX, Y).
+
+draw_point(X, Y) :- map_size(W, H),
+					X < W + 1,
+					X > 0,
+					Y < H + 1,
+					Y > 0,
+					map_object(X, Y, 'P'),
+					map_object(X, Y, 'R'), !,
+					write('P'),
+					NewX is X+1,
+					draw_point(NewX, Y).
+
+draw_point(X, Y) :- map_size(W, H),
+					X < W + 1,
+					X > 0,
+					Y < H + 1,
+					Y > 0,
+					map_object(X, Y, 'P'),
+					map_object(X, Y, 'H'), !,
+					write('P'),
+					NewX is X+1,
+					draw_point(NewX, Y).
+
+draw_point(X, Y) :- map_size(W, H),
+					X < W + 1,
+					X > 0,
+					Y < H + 1,
+					Y > 0,
+					map_object(X, Y, 'P'),
+					map_object(X, Y, 'Q'), !,
+					write('P'),
+					NewX is X+1,
+					draw_point(NewX, Y).
+
+draw_point(X, Y) :- map_size(W, H),
+					X < W + 1,
+					X > 0,
+					Y < H + 1,
+					Y > 0,
+					map_object(X, Y, 'P'),
+					map_object(X, Y, '='), !,
+					write('P'),
+					NewX is X+1,
+					draw_point(NewX, Y).
+
+/* Mencetak objek yang tidak tertimpa simbol pemain */
+draw_point(X, Y) :- map_size(W, H),
+					X < W + 1,
+					X > 0,
+					Y < H + 1,
+					Y > 0,
+					map_object(X, Y, Obj), !,
+					write(Obj),
+					NewX is X+1,
+					draw_point(NewX, Y).
+
+/* Mencetak lokasi kosong */
+draw_point(X, Y) :- map_size(W, H),
+					X < W + 1,
+					X > 0,
+					Y < H + 1,
+					Y > 0,
+					(\+ map_object(X, Y, _)),
+					write('-'),
+					NewX is X+1,
+					draw_point(NewX, Y).
+
+/* Mencetak map secara rekursif mulai dari point (0,0) */
+draw_map :- draw_point(0, 0).
---
+++ move.pl
@@ -0,0 +1,105 @@
+/* File move.pl */
+/* Berisi aturan perpindahan pemain (exploration mechanism) */
+
+/* Pesan ketika menabrak sesuatu */
+hit_fence :-
+    write('You hit a fence, use \'map.\' to see where you are right now').
+
+hit_water :-
+    write('You can\'t get into water, use \'map.\' to see where you are right now').
+
+/* Bergerak ke utara (atas) */
+n :-       
+	isStart(true),
+    map_object(X,Y,'P'),
+    YNew is Y-1,
+    map_object(X, YNew, 'o'), !, hit_water, fail.
+
+n :-       
+	isStart(true),
+    map_object(X,Y,'P'),
+    YNew is Y-1,
+    map_size(_, H),
+    YNew > 0, YNew =< H, !,
+    retract(map_object(X, Y, 'P')),
+    assertz(map_object(X,YNew,'P')),
+    write('You moved north.').
+
+n :-
+    isStart(false), !,
+	write('Game has not started, use \"start.\" to play the game"').
+
+n :-
+    hit_fence.
+
+/* Bergerak ke timur (kanan) */
+e :- 
+	isStart(true),
+    map_object(X,Y,'P'),
+    XNew is X+1,
+    map_object(XNew, Y, 'o'), !, hit_water, fail.
+
+e :-
+	isStart(true),
+    map_object(X,Y,'P'),
+    XNew is X+1,
+    map_size(W, _),
+    XNew > 0, XNew =< W, !,
+    retract(map_object(X, Y, 'P')),
+    assertz(map_object(XNew,Y,'P')),
+    write('You moved east.').
+
+e :-
+    isStart(false), !,
+	write('Game has not started, use \"start.\" to play the game"').
+
+e :-
+    hit_fence.
+
+/* Bergerak ke selatan (bawah) */
+s :- 
+	isStart(true),
+    map_object(X,Y,'P'),
+    YNew is Y+1,
+    map_object(X, YNew, 'o'), !, hit_water, fail.
+
+s :- 
+	isStart(true),
+    map_object(X,Y,'P'),
+    YNew is Y+1,
+    map_size(_, H),
+    YNew > 0, YNew =< H, !,
+    retract(map_object(X, Y, 'P')),
+    assertz(map_object(X,YNew,'P')),
+    write('You moved south.').
+
+s :-
+    isStart(false), !,
+	write('Game has not started, use \"start.\" to play the game"').
+
+s :-
+    hit_fence.
+
+/* Bergerak ke barat(kiri) */
+w :- 
+	isStart(true),
+    map_object(X,Y,'P'),
+    XNew is X-1,
+    map_object(XNew, Y, 'o'), !, hit_water, fail.
+
+w :- 
+	isStart(true),
+    map_object(X,Y,'P'),
+    XNew is X-1,
+    map_size(W, _),
+    XNew > 0, XNew =< W, !,
+    retract(map_object(X, Y, 'P')),
+    assertz(map_object(XNew,Y,'P')),
+    write('You moved west.').
+
+w :-
+    isStart(false), !,
+	write('Game has not started, use \"start.\" to play the game"').
+
+w :-
+    hit_fence.
