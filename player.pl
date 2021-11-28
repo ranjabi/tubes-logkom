3b6ed20dd87bca789b8d2692c67fcb6a948e6cca
wmk567 <16520402@std.stei.itb.ac.id>
2021-11-28 11:14:43 +0700

    perbaikin sedikit

--- player.pl
+++ player.pl
@@ -56,7 +56,7 @@ level_up_player:-
         M1 is M + 1,
         assertz(playerLevelUp(M1,Exp21)),
         retract(playerLevelUp(M,Exp2)),
-        write('your level player : '),write('N1')
+        write('your level player : '),write(N1)
     ).
 
 
